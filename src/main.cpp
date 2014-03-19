25048080ab692d6a6a8e975820fffb90cae55b57
markus <solbach.m@gmail.com>
2014-03-19 17:50:39 +0100

    eigen tested and cmake adjusted

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+#include <eigen3/Eigen/Dense>
+
 
 
 int main(int argc, const char* argv[])
 {
-    std::cout << "Hello World" << std::endl;
+    std::cout << "Hello Eigen" << std::endl;
+
+    Eigen::Matrix2d a;
+    a << 1, 2, 8, 9;
+
+    std::cout << a << std::endl;
+
+    std::cout << "Adiós Eigen" << std::endl;
     return 0; 
 }
